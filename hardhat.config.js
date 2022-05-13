8de5e52c683a9d1b84d57889e63de9e9a50fab48
Kamal <kamalproacc@gmail.com>
2022-05-13 13:03:21 +0530

    config file

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,30 @@
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
+require('@nomiclabs/hardhat-ethers');
 module.exports = {
-  solidity: "0.7.3",
-};
+  defaultNetwork: "rinkeby",
+  networks: {
+    hardhat: {
+    },
+    rinkeby: {
+      url: process.env.RINKEBY_URL || "",
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    }
+  },
+  solidity: {
+    version: "0.8.0",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+  mocha: {
+    timeout: 40000
+  }
+}
